3d048c826697d1068465f932700daea1d82a5399
Amador Fernandez <Amadorfl72@gmail.com>
2026-06-24 19:02:05 +0200

    TRIOFSND-39: Implementar reinicio de partida al pulsar 'Volver a jugar'

---
+++ src/js/i18n/strings.es-ES.js
@@ -0,0 +1,22 @@
+/**
+ * Spanish (es-ES) string resources for DinoQuiz.
+ * Keys are used throughout the app to allow future i18n without refactor.
+ */
+
+export const strings = {
+  app_title: 'DinoQuiz',
+  play_button: '¡Jugar!',
+  replay_button: 'Volver a jugar',
+  replay_aria: 'Volver a jugar: empezar una nueva partida',
+  next_button: 'Siguiente',
+  options_aria: 'Opciones de respuesta',
+  results_title: '¡Has terminado!',
+  results_score: 'Has acertado {score}/10',
+  results_new_best: '¡Nueva mejor puntuación!',
+  results_message_0_3: '¡Buen intento! Los dinosaurios son muy antiguos y hay mucho que descubrir. ¡Prueba otra vez!',
+  results_message_4_6: '¡Muy bien! Sabes un montón sobre dinosaurios. ¡Sigue jugando para aprender más!',
+  results_message_7_8: '¡Genial! Eres casi un experto en dinosaurios. ¡A ver si superas tu puntuación!',
+  results_message_9_10: '¡Increíble! Eres un verdadero paleontólogo. ¡Vuelve a jugar y demuestra que lo sabes todo!',
+  feedback_correct: '¡Muy bien! 🎉',
+  feedback_incorrect: '¡Casi! La respuesta correcta era: {correct}',
+};
