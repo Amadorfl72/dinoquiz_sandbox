a1ec91bbe1cc14f68d5998a4d9d8cada0b1cf9b7
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-01 17:01:40 +0200

    test: Fix: TRIOFSND-32: Implement Results Screen UI

--- app/src/androidTest/java/com/example/app/ResultsScreenTest.kt
+++ app/src/androidTest/java/com/example/app/ResultsScreenTest.kt
@@ -1,24 +1,39 @@
-package com.example.app
-
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.assertHeightIsAtLeast
-import androidx.compose.ui.unit.dp
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class ResultsScreenTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val activityRule = ActivityScenarioRule(ResultsActivity::class.java)
 
     @Test
     fun volverAJugarButton_hasMinimumHeightOf48dp() {
-        composeTestRule.setContent {
-            ResultsScreen()
-        }
+        onView(withText("Volver a jugar"))
+            .check(matches(hasMinimumHeight(48)))
+    }
 
-        composeTestRule.onNodeWithText("Volver a jugar")
-            .assertHeightIsAtLeast(48.dp)
+    private fun hasMinimumHeight(minHeightDp: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun matchesSafely(view: View): Boolean {
+                val density = view.resources.displayMetrics.density
+                val minHeightPx = (minHeightDp * density + 0.5f).toInt()
+                return view.height >= minHeightPx
+            }
+
+            override fun describeTo(description: Description) {
+                description.appendText("view height should be at least $minHeightDp dp")
+            }
+        }
     }
 }
