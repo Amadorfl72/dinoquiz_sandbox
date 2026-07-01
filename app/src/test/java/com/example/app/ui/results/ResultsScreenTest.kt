9b4e983f60364663da7d8fb2e5631ad8ded7649d
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-01 16:27:53 +0200

    test: Fix: TRIOFSND-32: Implement Results Screen UI

---
+++ app/src/test/java/com/example/app/ui/results/ResultsScreenTest.kt
@@ -0,0 +1,25 @@
+package com.example.app.ui.results
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.assertHeightIsAtLeast
+import androidx.compose.ui.unit.dp
+import org.junit.Rule
+import org.junit.Test
+
+class ResultsScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testVolverAJugarButtonHeight() {
+        composeTestRule.setContent {
+            ResultsScreen()
+        }
+
+        composeTestRule
+            .onNodeWithText("Volver a Jugar")
+            .assertHeightIsAtLeast(48.dp)
+    }
+}
