3ccf62544ac34a09d4bada2957ea94508c071da9
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-01 16:02:12 +0200

    TRIOFSND-26: Update FunFact component with kid-friendly styling and security fixes

--- src/components/FunFact.js
+++ src/components/FunFact.js
@@ -5,10 +5,19 @@ const FunFact = ({ fact, dinosaurImage, onNext }) => {
   return (
     <div className="fun-fact-container">
       <div className="fun-fact-content">
-        <img src={dinosaurImage} alt="Dinosaur" className="dinosaur-image" />
+        <img 
+          src={dinosaurImage} 
+          alt="Dinosaur" 
+          className="dinosaur-image" 
+          data-testid="dinosaur-image"
+        />
         <p className="fun-fact-text">{fact}</p>
       </div>
-      <button className="next-button" onClick={onNext}>
+      <button 
+        className="next-button" 
+        onClick={onNext}
+        data-testid="next-button"
+      >
         Next
       </button>
     </div>
