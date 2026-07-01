af59b2a9a70bb788bb7b90435ae4b0143581b2af
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-01 16:13:44 +0200

    Implement offline first load fallback message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import OfflineFirstLoadMessage from './components/OfflineFirstLoadMessage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <OfflineFirstLoadMessage />
+      {/* Rest of your app */}
+    </div>
+  );
+}
+
+export default App;
