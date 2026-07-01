22b64257ced64d62881ba96228e3830d899dc366
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-01 16:28:57 +0200

    TRIOFSND-54: Update analytics integration with security fixes

--- src/analytics/analyticsMiddleware.js
+++ src/analytics/analyticsMiddleware.js
@@ -1,18 +1,24 @@
 import { trackAppOpen, trackTooltipShown, trackTooltipDismissed } from './analyticsService';
 
 const analyticsMiddleware = (store) => (next) => (action) => {
-  switch (action.type) {
-    case 'APP_OPENED':
-      trackAppOpen(action.payload.isFirstOpen);
-      break;
-    case 'TOOLTIP_SHOWN':
-      trackTooltipShown();
-      break;
-    case 'TOOLTIP_DISMISSED':
-      trackTooltipDismissed();
-      break;
-    default:
-      break;
+  try {
+    switch (action.type) {
+      case 'APP_OPENED':
+        if (typeof action.payload?.isFirstOpen === 'boolean') {
+          trackAppOpen(action.payload.isFirstOpen);
+        }
+        break;
+      case 'TOOLTIP_SHOWN':
+        trackTooltipShown();
+        break;
+      case 'TOOLTIP_DISMISSED':
+        trackTooltipDismissed();
+        break;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.error('[Analytics Middleware Error]', error);
   }
 
   return next(action);
