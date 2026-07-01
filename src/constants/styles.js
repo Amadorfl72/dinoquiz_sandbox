539ed144558879c58eb72d3f60e337563274acb3
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-01 16:51:18 +0200

    TRIOFSND-59: Increase answer option touch target to 48x48dp on small devices

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -3,18 +3,21 @@ export const responsive = {
     questionText: 20,
     optionText: 18,
     optionButtonHeight: 48,
+    optionButtonMinWidth: 48,
     dinoImageSize: 150,
   },
   medium: {
     questionText: 24,
     optionText: 20,
     optionButtonHeight: 60,
+    optionButtonMinWidth: 60,
     dinoImageSize: 200,
   },
   large: {
     questionText: 28,
     optionText: 22,
     optionButtonHeight: 70,
+    optionButtonMinWidth: 70,
     dinoImageSize: 250,
   },
 };
