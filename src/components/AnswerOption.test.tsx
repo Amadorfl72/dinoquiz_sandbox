9cfb08623c9bcbe3862b8d9b93b8f796b21d924d
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-01 16:24:33 +0200

    test: TRIOFSND-20: Implement Double Tap Debounce

---
+++ src/components/AnswerOption.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { AnswerOption } from '../AnswerOption';
+
+describe('AnswerOption Component - Double Tap Debounce', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it('prevents multiple answers if the same option is tapped twice quickly', () => {
+    const handleAnswer = jest.fn();
+    render(<AnswerOption optionId="A" onAnswer={handleAnswer} debounceTime={500} />);
+
+    const button = screen.getByRole('button');
+
+    act(() => {
+      fireEvent.click(button);
+    });
+    
+    act(() => {
+      fireEvent.click(button);
+    });
+
+    expect(handleAnswer).toHaveBeenCalledTimes(1);
+    expect(handleAnswer).toHaveBeenCalledWith('A');
+  });
+
+  it('allows answering again after the debounce time', () => {
+    const handleAnswer = jest.fn();
+    render(<AnswerOption optionId="A" onAnswer={handleAnswer} debounceTime={500} />);
+
+    const button = screen.getByRole('button');
+
+    act(() => {
+      fireEvent.click(button);
+    });
+
+    act(() => {
+      jest.advanceTimersByTime(500);
+    });
+
+    act(() => {
+      fireEvent.click(button);
+    });
+
+    expect(handleAnswer).toHaveBeenCalledTimes(2);
+  });
+});
