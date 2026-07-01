c5685c1d44b0fabb59c6261ef26c188724e1d570
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-01 17:05:34 +0200

    fix: TRIOFSND-26 Build Fun Fact UI component
    - Add missing test tags for UI elements
    - Ensure next button meets minimum height requirement
    - Fix package name inconsistencies in test files

--- src/components/FunFactScreen.kt
+++ src/components/FunFactScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
