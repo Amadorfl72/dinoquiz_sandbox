4dd341affbf4ddc67ae6e3863289ff19165da5d4
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-01 16:36:36 +0200

    test: TRIOFSND-53: PWA Setup and Performance Optimization

---
+++ __tests__/pwa/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/pwa/**/*.test.js'],
+  testTimeout: 60000,
+  collectCoverage: false,
+  verbose: true,
+  transform: {},
+};
