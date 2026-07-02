19ba7e2b455d7aedd4eab5258ed58e4e87e1156b
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 10:35:39 +0200

    Implement click handler for '¡Jugar!' button with navigation and analytics event

---
+++ src/pages/Game.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Game = () => (
+  <div style={styles.container}>
+    <h1>Game Screen</h1>
+    {/* Game content will be added here */}
+  </div>
+);
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '20px',
+  },
+};
+
+export default Game;
