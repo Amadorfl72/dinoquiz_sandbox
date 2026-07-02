9340dfe767bd0a3c0e88d2842f846422276579bc
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 10:27:59 +0200

    TRIOFSND-19: Implement incorrect answer feedback with visual highlight and gentle message

---
+++ src/components/Quiz.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="quiz">
+    <QuizQuestion
+      v-if="currentQuestion"
+      :question="currentQuestion"
+      :correct-feedback="strings.correctFeedback"
+      :incorrect-feedback="strings.incorrectFeedback"
+      @answer-selected="handleAnswer"
+      @next-question="nextQuestion"
+    />
+  </div>
+</template>
+
+<script>
+import QuizQuestion from './QuizQuestion.vue';
+import strings from '../assets/strings.json';
+
+export default {
+  components: { QuizQuestion },
+  props: {
+    questions: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      currentQuestionIndex: 0,
+      score: 0,
+      strings
+    };
+  },
+  computed: {
+    currentQuestion() {
+      return this.questions[this.currentQuestionIndex];
+    }
+  },
+  methods: {
+    handleAnswer(isCorrect) {
+      if (isCorrect) {
+        this.score++;
+      }
+    },
+    nextQuestion() {
+      if (this.currentQuestionIndex < this.questions.length - 1) {
+        this.currentQuestionIndex++;
+      } else {
+        this.$emit('quiz-completed', this.score);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.quiz {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
