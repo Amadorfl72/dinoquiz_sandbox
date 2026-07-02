3c608a6e68ce011f50887cb83e5342ae2df550a8
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 18:25:22 +0200

    Fix double tap debounce implementation to only prevent rapid clicks on same option

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { debounce } from '../utils/debounce';
 
 const AnswerOption = ({ option, onSelect, isSelected, isCorrect }) => {
+  const lastSelectedOptionId = useRef(null);
+
   const handleSelect = debounce(() => {
     if (!isSelected) {
       onSelect(option);
+      lastSelectedOptionId.current = option.id;
     }
   }, 300);
+
+  const handleClick = () => {
+    if (option.id === lastSelectedOptionId.current) {
+      handleSelect();
+    } else {
+      onSelect(option);
+      lastSelectedOptionId.current = option.id;
+    }
+  };
 
   return (
     <button
       className={`answer-option ${isSelected ? (isCorrect ? 'correct' : 'incorrect') : ''}`}
-      onClick={handleSelect}
+      onClick={handleClick}
       disabled={isSelected}
     >
       {option.text}
