366f5f4dc8d879fe6788fdaa113c0c3719266df2
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 11:26:49 +0200

    Fix: TRIOFSND-32 - Adjust 'Volver a jugar' button height to meet minimum requirement

---
+++ src/components/ResultsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ResultsScreen.css';
+
+const ResultsScreen = () => {
+  return (
+    <div className="results-screen">
+      <h1>Resultados</h1>
+      <button className="results-screen-button">Volver a jugar</button>
+    </div>
+  );
+};
+
+export default ResultsScreen;
