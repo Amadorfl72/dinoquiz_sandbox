5f1b24b9e095489dd35dc269d5a3622af1799824
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 10:35:37 +0200

    Implement click handler for '¡Jugar!' button with navigation and analytics event

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Game from './pages/Game';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/game" element={<Game />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
