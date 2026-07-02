eda1c6f09905f66af10e302bc0357a6745f54258
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 10:22:08 +0200

    TRIOFSND-30: Trigger fun_fact_viewed event on screen load
    - Updated analytics service to use structured logging
    - Modified FunFactScreen to use analyticsLogger service
    - Added security considerations to README

--- src/services/analytics.js
+++ src/services/analytics.js
@@ -1,7 +1,8 @@
+import { analyticsLogger } from './analyticsLogger';
+
 export const logEvent = (eventName, eventParams = {}) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log(`[Analytics] ${eventName}`, eventParams);
-  }
-  // TODO: Implement actual analytics service integration
-  // For example: Firebase Analytics, Plausible, etc.
+  analyticsLogger.emit({
+    event: eventName,
+    ...eventParams
+  });
 };
