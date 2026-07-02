08af8222dddc1f13477f10a1ceb4978bad5a75a6
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 18:19:27 +0200

    TRIOFSND-39: Implement play again handler with state reset and question selection

--- src/utils/gameLogic.js
+++ src/utils/gameLogic.js
@@ -1,7 +1,21 @@
-export const resetGameState = () => {
-  // Reset game state logic here
+import { selectQuestions } from '../services/questionService';
+
+export const resetGameState = (state) => {
+  return {
+    ...state,
+    score: 0,
+    currentQuestionIndex: 0,
+    answeredQuestions: [],
+    screen: 'playing'
+  };
 };
 
-export const selectQuestions = () => {
-  // Select new set of questions logic here
+export const restartGame = (state) => {
+  const newState = resetGameState(state);
+  const questions = selectQuestions();
+  return {
+    ...newState,
+    questions,
+    currentQuestion: questions[0]
+  };
 };
