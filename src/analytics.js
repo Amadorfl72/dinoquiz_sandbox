cc3ae1d622e22ec7d87d8f460d43344ed41aa85f
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 10:15:24 +0200

    Implement client-side logging for 'game_completed' event

---
+++ src/analytics.js
@@ -0,0 +1,6 @@
+const logEvent = (eventName, eventData) => {
+  // Implementation to send event to backend endpoint
+  console.log(`Logging event: ${eventName}`, eventData);
+};
+
+export { logEvent };
