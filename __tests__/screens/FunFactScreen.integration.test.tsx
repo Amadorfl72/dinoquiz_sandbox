1e2a9f95de7f50de0cd931ab3d43a3db44cdcf06
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 18:30:58 +0200

    test: TRIOFSND-30: Trigger fun_fact_viewed event on screen load

--- __tests__/screens/FunFactScreen.integration.test.tsx
+++ __tests__/screens/FunFactScreen.integration.test.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import { FunFactScreen } from '../../src/screens/FunFactScreen';
+import FunFactScreen from '../../src/screens/FunFactScreen';
 
 describe('TRIOFSND-30: FunFactScreen integration', () => {
   it('renders the screen and triggers fun_fact_viewed log on mount', () => {
     const logSpy = jest.spyOn(console, 'log').mockImplementation();
 
-    const { getByTestId } = render(<FunFactScreen />);
+    const { getByTestId } = render(
+      <FunFactScreen route={{ params: { funFact: { text: 'Test fun fact' } } }} />
+    );
 
     expect(getByTestId('fun-fact-screen')).toBeTruthy();
 
