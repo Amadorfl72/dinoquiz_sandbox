e14ada8d965af823acb858af2f597d19f3680849
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 18:21:07 +0200

    Fix field name inconsistencies between JS logging and Python aggregator

--- src/analytics/logEvents.js
+++ src/analytics/logEvents.js
@@ -1,15 +1,15 @@
 // Logs structured events for question answered and feedback shown
 const logQuestionAnswered = (questionId, isCorrect, timeToAnswerMs) => {
   console.log('question_answered', {
-    questionId,
-    isCorrect,
-    timeToAnswerMs
+    question_id: questionId,
+    success: isCorrect,
+    time_to_answer_ms: timeToAnswerMs
   });
   // TODO: Integrate with analytics service (Firebase/Plausible/Matomo)
 };
 
 const logFeedbackShown = (questionId) => {
-  console.log('feedback_shown', { questionId });
+  console.log('feedback_shown', { question_id: questionId });
   // TODO: Integrate with analytics service
 };
 
