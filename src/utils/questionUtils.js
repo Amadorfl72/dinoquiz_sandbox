0d3c4202190ba276535929085c04d38e44676e3b
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 18:21:05 +0200

    TRIOFSND-38: Align question data structure between JS and TS implementations

--- src/utils/questionUtils.js
+++ src/utils/questionUtils.js
@@ -15,28 +15,26 @@ export const selectRandomQuestions = (allQuestions, count = 10) => {
   return selectedQuestions;
 };
 
-export const shuffleAnswers = (question) => {
-  // Create a copy of the answers array to avoid mutating the original
-  const answersCopy = [...question.answers];
-  const correctAnswer = question.correctAnswer;
+export const shuffleQuestionOptions = (question) => {
+  // Create a copy of the options array to avoid mutating the original
+  const optionsCopy = [...question.options];
+  
+  // Find the correct option before shuffling
+  const correctOption = optionsCopy.find(option => option.isCorrect);
 
-  // Shuffle the answers array
-  for (let i = answersCopy.length - 1; i > 0; i--) {
+  // Shuffle the options array
+  for (let i = optionsCopy.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [answersCopy[i], answersCopy[j]] = [answersCopy[j], answersCopy[i]];
+    [optionsCopy[i], optionsCopy[j]] = [optionsCopy[j], optionsCopy[i]];
   }
-
-  // Update the correctAnswer index
-  const newCorrectAnswerIndex = answersCopy.indexOf(correctAnswer);
 
   return {
     ...question,
-    answers: answersCopy,
-    correctAnswerIndex: newCorrectAnswerIndex
+    options: optionsCopy
   };
 };
 
 export const prepareQuestionsForGame = (allQuestions, count = 10) => {
   const selectedQuestions = selectRandomQuestions(allQuestions, count);
-  return selectedQuestions.map(question => shuffleAnswers(question));
+  return selectedQuestions.map(question => shuffleQuestionOptions(question));
 };
