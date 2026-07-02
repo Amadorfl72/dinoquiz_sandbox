915e645151f8a833369ef401816207b77ddff3b0
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 18:26:47 +0200

    Separate caches for different asset types in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,43 @@
-const CACHE_NAME = 'dinoquiz-v1';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/styles/main.css',
-  '/scripts/app.js',
-  '/images/dino-mascot.png',
-  '/audio/positive.mp3',
-  '/audio/neutral.mp3',
-  '/data/questions.json'
-];
+const CACHE_NAMES = {
+  APP_SHELL: 'dinoquiz-app-shell-v1',
+  ASSETS: 'dinoquiz-assets-v1',
+  AUDIO: 'dinoquiz-audio-v1',
+  IMAGES: 'dinoquiz-images-v1',
+  QUESTIONS: 'dinoquiz-questions-v1'
+};
+
+const ASSETS_TO_CACHE = {
+  APP_SHELL: [
+    '/',
+    '/index.html',
+    '/styles/main.css',
+    '/scripts/app.js'
+  ],
+  ASSETS: [
+    '/styles/main.css',
+    '/scripts/app.js'
+  ],
+  AUDIO: [
+    '/audio/positive.mp3',
+    '/audio/neutral.mp3'
+  ],
+  IMAGES: [
+    '/images/dino-mascot.png'
+  ],
+  QUESTIONS: [
+    '/data/questions.json'
+  ]
+};
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    Promise.all([
+      caches.open(CACHE_NAMES.APP_SHELL).then((cache) => cache.addAll(ASSETS_TO_CACHE.APP_SHELL)),
+      caches.open(CACHE_NAMES.ASSETS).then((cache) => cache.addAll(ASSETS_TO_CACHE.ASSETS)),
+      caches.open(CACHE_NAMES.AUDIO).then((cache) => cache.addAll(ASSETS_TO_CACHE.AUDIO)),
+      caches.open(CACHE_NAMES.IMAGES).then((cache) => cache.addAll(ASSETS_TO_CACHE.IMAGES)),
+      caches.open(CACHE_NAMES.QUESTIONS).then((cache) => cache.addAll(ASSETS_TO_CACHE.QUESTIONS))
+    ])
   );
 });
 
@@ -25,7 +49,7 @@ self.addEventListener('fetch', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+  const cacheWhitelist = Object.values(CACHE_NAMES);
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
