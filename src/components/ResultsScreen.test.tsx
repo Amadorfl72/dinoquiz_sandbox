99f1eb9f9bfd2aa6ae994e412db02485398a0ef2
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-02 10:37:20 +0200

    TRIOFSND-36: Fix import paths and endpoint in game completed logging

--- src/components/ResultsScreen.test.tsx
+++ src/components/ResultsScreen.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import ResultsScreen from './ResultsScreen';
-import * as gameCompletedLogger from '../logging/gameCompletedLogger';
+import * as gameCompletedLogger from '../logging';
 
 describe('TRIOFSND-36: ResultsScreen', () => {
   it('logs game_completed event upon reaching the results screen', () => {
