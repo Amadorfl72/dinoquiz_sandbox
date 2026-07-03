480c2720b7a1760496da3dd54d93f7d9ff825704
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-03 12:00:13 +0200

    TRIOFSND-20: Fix double tap debounce logic to prevent multiple answers on same option

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -3,22 +3,28 @@ import { debounce } from '../utils/debounce';
 
 const AnswerOption = ({ option, onSelect, isSelected, isCorrect }) => {
   const lastSelectedOptionId = useRef(null);
-
-  const handleSelect = useRef(debounce((opt) => {
-    if (!isSelected) {
-      onSelect(opt);
-      lastSelectedOptionId.current = opt.id;
-    }
-  }, 300)).current;
+  const isDebouncing = useRef(false);
 
   const handleClick = () => {
-    if (option.id === lastSelectedOptionId.current) {
-      // If clicking the same option again, use debounced handler
-      handleSelect(option);
-    } else {
-      // If clicking a different option, call onSelect directly
+    // If already selected or currently debouncing, ignore
+    if (isSelected || isDebouncing.current) {
+      return;
+    }
+
+    // For first click or different option, call onSelect immediately
+    if (lastSelectedOptionId.current !== option.id) {
       onSelect(option);
       lastSelectedOptionId.current = option.id;
+      return;
+    }
+
+    // For same option click, debounce
+    if (!isDebouncing.current) {
+      isDebouncing.current = true;
+      onSelect(option);
+      setTimeout(() => {
+        isDebouncing.current = false;
+      }, 300);
     }
   };
 
