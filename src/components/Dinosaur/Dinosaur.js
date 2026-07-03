285bea25584623ecc446c20c4813e287bed9b0ee
Amador Fernandez <Amadorfl72@gmail.com>
2026-07-03 12:26:54 +0200

    fix(TRIOFSND-27): Implement image fallback placeholder with dinosaur default
    - Added Dinosaur component with fallback image
    - Fixed test imports to use correct path
    - Ensured text remains legible when placeholder is displayed

--- src/components/Dinosaur/Dinosaur.js
+++ src/components/Dinosaur/Dinosaur.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import dinosaurPlaceholder from '../../assets/dinosaur-placeholder.png';
 import './Dinosaur.css';
 
@@ -11,19 +10,14 @@ const Dinosaur = ({ imageUrl, altText }) => {
 
   return (
     <div className="dinosaur-container">
-      <img
-        src={imageUrl}
+      <img 
+        src={imageUrl} 
         alt={altText}
         className="dinosaur-image"
         onError={handleImageError}
       />
     </div>
   );
-};
-
-Dinosaur.propTypes = {
-  imageUrl: PropTypes.string.isRequired,
-  altText: PropTypes.string.isRequired
 };
 
 export default Dinosaur;
